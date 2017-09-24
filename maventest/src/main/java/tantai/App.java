15316df9591f1bac91ee4674de61a59e39bebc13
tai8292 <tai828292@gmail.com>
2017-09-25 01:51:08 +0700

    update

--- maventest/src/main/java/tantai/App.java
+++ maventest/src/main/java/tantai/App.java
@@ -1,5 +1,6 @@
 package tantai;
-
+import java.util.Scanner;
+import org.joda.time.LocalTime;
 /**
  * Hello world!
  *
@@ -10,12 +11,20 @@ public final class App {
  * Constructor
  */
     private App() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Input a: ");
+        int a = in.nextInt();
+        System.out.println(a);
     }
 /**
  * Hello world!
  *@param args is args
  */
     public static void main(final String[] args) {
-        System.out.println("Hello World!");
+        App r = new App();
+	LocalTime currentTime = new LocalTime();
+	System.out.println("The current local time is: " + currentTime);
+	Greeter greeter = new Greeter();
+	System.out.println(greeter.sayHello()); 
     }
 }
---
+++ maventest/src/main/java/tantai/Greeter.java
@@ -0,0 +1,7 @@
+package tantai;
+
+public class Greeter {
+    public String sayHello() {
+        return "Hello world!";
+    }
+}
