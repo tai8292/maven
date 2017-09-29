0c0de0a9016bb090315d7e94a421cde490f5ea1d
tai8292 <tai828292@gmail.com>
2017-09-29 14:42:09 +0700

    update

--- maventest/src/main/java/tantai/App.java
+++ maventest/src/main/java/tantai/App.java
@@ -2,6 +2,7 @@ package tantai;
 import java.util.Scanner;
 import org.joda.time.LocalTime;
 import org.joda.time.LocalDate;
+import java.util.Date;
 /**
  * Hello world!
  *
@@ -19,23 +20,14 @@ public final class App {
     }
 /**
  * Hello world!
- *@param args is args
+ *@param args is
  */
     public static void main(final String[] args) {
-        Scanner in = new Scanner(System.in);
-        App r = new App();
         LocalTime currentTime = new LocalTime();
         LocalDate currentDate = new LocalDate();
         System.out.println("The current local time is: " + currentTime);
         System.out.println("The current local date is: " + currentDate);
-
-        Greeter greeter = new Greeter();
-        System.out.println(greeter.sayHello());
-        greeter.sayGoodbye();
-
-        System.out.print("\nInput condition: ");
-        String condition = in.next();
-        SayHello s = new SayHello();
-        s.isTrue(condition);
+        Date date = new Date();
+        System.out.println("The date: " + date);
     }
 }
